88f34129df7c687d7f347b6cf7d88fb2c44d4143
De Carli <pedrodecarlisilva@gmail.com>
2025-07-12 00:27:47 -0300

    corrigir acentuação nos nomes dos produtos no dicionário de chaves

--- pages/estoque.py
+++ pages/estoque.py
@@ -16,7 +16,7 @@ st.header('FEEDBACK DE VENDAS')
 
 df_sales = pd.read_sql_table('historico_de_vendas', con=create_engine(st.secrets['connections']['postgres']['url']))
 
-product_keys = {'COCA': 'coca', 'ÁGUA': 'agua', 'GUARANÁ':'guarana', 'SUCO': 'suco', 'CHOPP': 'chopp', 'PROMO CHOPP': 'promo_chopp', 'VINHO':'vinho', 'PROMO VINHO':'promo_vinho', 'GUARAVITA':'guaravita'}
+product_keys = {'COCA': 'coca', 'AGUA': 'agua', 'GUARANA':'guarana', 'SUCO': 'suco', 'CHOPP': 'chopp', 'PROMO CHOPP': 'promo_chopp', 'VINHO':'vinho', 'PROMO VINHO':'promo_vinho', 'GUARAVITA':'guaravita'}
 
 with st.expander(expanded=True, label='Produtos em Estoque'):
     product_columns = st.columns(3, vertical_alignment='top') if num_products >= 3 else st.columns(num_products, vertical_alignment='center')
@@ -40,8 +40,8 @@ with st.expander(expanded=True, label='Registro de Vendas'):
                  column_config={
                      'id': st.column_config.NumberColumn('Nº da Venda'),
                      'coca': st.column_config.NumberColumn('COCA'),
-                     'agua': st.column_config.NumberColumn('ÁGUA'),
-                     'guarana': st.column_config.NumberColumn('GUARANÁ'),
+                     'agua': st.column_config.NumberColumn('AGUA'),
+                     'guarana': st.column_config.NumberColumn('GUARANA'),
                      'suco': st.column_config.NumberColumn('SUCO'),
                      'chopp': st.column_config.NumberColumn('CHOPP'),
                      'promo_chopp': st.column_config.NumberColumn('PROMO CHOPP'),
